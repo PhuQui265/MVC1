1ddb9a9223615e42683a43027dbb988577479d3b
PhuQui265 <phuquin46@gmail.com>
2025-04-23 08:48:46 +0700

    up first

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+    'base' => '/MVC-demo/',
+    'baseURL' => 'http://localhost/MVC-demo/',
+    'assets' => '/MVC-demo/',
+    'db' => array(
+        'host'     => 'localhost',
+        'name'     => 'ProductDB',
+        'username' => 'root',
+        'password' => ''
+    )
+);
